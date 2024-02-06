d3c47776f1f88c47baa0d5b0e51928b319e333e8
Jakub "Xapixowy" Chodziński <xapixowy@gmail.com>
2024-02-06 22:03:58 +0100

    delete defineProps from imports

--- src/components/tvshows_components/EpisodeCarousel.vue
+++ src/components/tvshows_components/EpisodeCarousel.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, ref } from 'vue'
+import { computed, ref } from 'vue'
 import { IconChevronRight, IconChevronLeft } from '@tabler/icons-vue'
 import EpisodeCard from './EpisodeCard.vue'
 
